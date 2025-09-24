d76d9917021e13128ae44a90fc6a95b44fe82653
ouicmoijim <jimtrepanier2005@gmail.com>
2025-09-24 14:12:32 -0400

    Organisé les fichier

---
+++ ScriptMatlab/calcul_partie_2.m
@@ -0,0 +1,42 @@
+
+%% Deuxième ordre (n=2)
+C = 4.7 * 10^(-9); % posé
+w = 15000 * 2 * pi; 
+R = 1100; % posé pour avoir un q proche d'un entier
+
+
+q = 1/(R*w*2*C);
+
+m = 1/(w^2 * R^2 * C^2 * q);
+
+%% Premier Ordre (n=1)
+C = 2.2*10^(-9); % posé
+
+R = 1/(w * C);
+
+%% Fonction de transfert et diagramme de Bode
+
+s=tf('s');%Déclaration de la variable
+
+%Les fréquences et fonction de transfert
+wc=15000 * 2 * pi;
+w3=(15000+6750) * 2 * pi;
+H = 1/(((s^2)/(wc^2) + s/wc + 1)*(s/wc+1));
+
+grid on
+hold on
+
+% Ajout des lignes pour voir notre bande-passante
+xline(wc,'r--','LineWidth',1.5);
+xline(w3,'r--','LineWidth',1.5);
+%Traçage du diagrame
+bode(H)
+
+[mag, ~] = bode(H, [wc w3]);
+mag = squeeze(mag);
+mag_db = 20*log10(mag);
+
+
+
+fprintf("À la fréquence de coupure, l'amplitude est %.2f.\nÀ la fin " + ...
+    "de la bande passante, l'amplitude est %.2f.\n",mag_db);
